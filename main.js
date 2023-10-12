8333f7190ca17f6ec80413cd4d6c4ec2d4dc577d
anisakholip <anisakholip@gmail.com>
2023-10-12 17:19:18 +0700

    add file.js

---
+++ main.js
@@ -0,0 +1,57 @@
+// console.log("hello wolrd")
+// //notifikasi
+// alert("ada notifikasi")
+// prompt("pinjem dong")
+
+// var promnet =" coding is easy!"
+// console.log(promnet)
+// var promnet =" coding is hard!"
+// console.log(promnet)
+
+// // pakai let biar kalo namanya sama ga error
+// let promnet = "coding is fun"
+// console.log(promnet)
+// let promnet = "coding not fun"
+// console.log(promnet)
+
+// // di js boleh pake koma boleh engga
+// // const promnet =" coding is my life"
+// // console.log(promnet)
+// // const promnet =" coding is my life"
+// // console.log(promnet)
+
+// // conditional statement
+// let totalPoin = prompt ("masukkan poin anda")
+// if(totalPoin > 100){
+// document.write("congrats");
+// }else{
+//     document.write("keep trying");
+// }
+
+// let x=6;
+// let y=3;
+// console.log(x<10 && y>1);
+// console.log(x<10 && y<1);
+// console.log(x==5||y==5);
+// console.log(x==6||y==5);
+// console.log(!x==y);
+
+
+// document.write(x<10 && y>1);
+// document.write("<br>"); 
+// document.write(x<10 && y<1);
+// document.write("<br>");
+// document.write(x==5||y==5);
+// document.write("<br>");
+// document.write(x==6||y==5);
+// document.write("<br>");
+// document.write(!x==y);
+
+let p =document.querySelector("p")
+let button =document.querySelector("button")
+let input= document.querySelector("input")
+button.addEventListener('click', function()){
+    let isi = input.value
+    console.log(isi)
+    p.innerHTML=isi
+}
